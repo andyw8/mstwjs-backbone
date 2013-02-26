4b4cfdedb27e2f7d563d626292a56f6ce0aa5631
Andy Waite <github.aw@andywaite.com>
2013-02-26 10:28:41 +0000

    New app

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails-backbone'
+gem 'jquery-rails'
+gem 'jquery-ui-rails'
+gem 'pg'
+gem 'rails', '3.2.11'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development do
+  gem 'rb-fsevent', '~> 0.9.1'
+end
+
+group :development, :test do
+  gem 'jasmine'
+  gem 'jasminerice'
+  gem 'rspec-rails'
+  gem 'guard-jasmine'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MstwjsBackbone::Application.config.secret_token = 'bd80ef6a10eb23434c13736ceeb561eeb936913ae4111c9e2638136a054740c987c017b70a5820f274a0fafc56c185a14caf1fd659dd159555813e7b4fa37ed2'
